881ecec64d636be4466ddc0111626457c1cb512b
hanfs390 <hanfs390@foxmail.com>
2018-12-19 22:19:28 +0800

    [init]

---
+++ MDK-ARM/RTE/_stm32_all/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32_all' 
+ * Target:  'stm32_all' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
